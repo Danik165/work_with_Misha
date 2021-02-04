2a860462b54e1a1d7e1fc4e9c7a54cde938fc6c7
dkamireddy@mail.ru <proHacker1>
2021-02-04 16:59:03 +0300

    bla

---
+++ Game2.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import time
+import random
+
+width = 500
+height = 400
+
+root = Tk()
+root.title('Snake')
+
+c = Canvas(root, wigth=500, height=400, bg='#003300')
+c.grid()
+
+c.focus_set()
+
+root.mainloop()
