cbc870cffa0179c19db30310c01794b464fc7bc6
Danik165 <dkamireddy@mail.ru>
2021-02-04 19:11:54 +0300

    1

--- Game2.py
+++ Game2.py
@@ -2,13 +2,10 @@ from tkinter import *
 import time
 import random
 
-width = 500
-height = 400
-
 root = Tk()
-root.title('Snake')
+root.title("Snake")
 
-c = Canvas(root, wigth=500, height=400, bg='#003300')
+c = Canvas(root, width=500, height=400, bg="#003300")
 c.grid()
 
 c.focus_set()
