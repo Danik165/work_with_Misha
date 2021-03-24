cb3b951cd4434a7d8d48d0693c4f863d0cc76049
Danik165 <dkamireddy@mail.ru>
2021-03-25 01:38:08 +0300

    1

--- Life1.py
+++ Life1.py
@@ -3,19 +3,11 @@ import numpy as np
 "Какой-то массив из блокнота"
 game_zone = np.array([
     ['_', '_', '_', '_'],
-    ['_', '_', '*', '_'],
-    ['_', '*', '_', '_'],
-    ['_', '_', '*', '_'],
+    ['_', '*', '*', '_'],
+    ['_', '*', '*', '_'],
+    ['_', '*', '*', '_'],
     ['_', '_', '_', '_'],
 ])
-
-
-def detect_alive_block(field):
-    for i in range(0, field.shape[0]):
-        for j in range(0, field.shape[1]):
-            if field[i][j] == '*':
-                middle_elem = field[i + 1][i + 1]
-                return field[i-1:i + 2, j-1:j + 2]
 
 
 def print_matrix(matrix):
@@ -23,10 +15,7 @@ def print_matrix(matrix):
         for j in range(0, matrix.shape[1]):
             print(matrix[i][j], end=' ')
         print()
-
-
-"Печать первого живого блока"
-print_matrix(detect_alive_block(game_zone))
+    print('_______')
 
 
 def step(matrix, neighbours=0):
@@ -42,7 +31,29 @@ def step(matrix, neighbours=0):
     return matrix
 
 
-"Полная печать"
-print_matrix(step(detect_alive_block(game_zone)))
+def detect_alive_block(field):
+    for i in range(1, field.shape[0] - 1):
+        for j in range(1, field.shape[1] - 1):
+            print_matrix(step(field[i - 1:i + 2, j - 1:j + 2]))
 
+
+"если убрать тут step, то будет обычный вывод матрицы 3х3"
+
+
+
+
+"Печать первого живого блока"
+print_matrix(detect_alive_block(game_zone))
+
+"Полная печать"
+
+
+def life(opperations):
+    pass
+    while opperations > 0:
+        print_matrix(step(detect_alive_block(game_zone)))
+        opperations -= 1
+
+
+#life(int(input()))
 #         "Таймер где-то тут"
