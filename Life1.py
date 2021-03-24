fffe7ae0ba1bcc4f9bd6c36cd8d97b3cbe8f5a54
Danik165 <dkamireddy@mail.ru>
2021-03-24 22:47:06 +0300

    1

--- Life1.py
+++ Life1.py
@@ -3,7 +3,7 @@ import numpy as np
 "Какой-то массив из блокнота"
 game_zone = np.array([
     ['_', '_', '_', '_'],
-    ['*', '_', '_', '_'],
+    ['_', '_', '*', '_'],
     ['_', '*', '_', '_'],
     ['_', '_', '*', '_'],
     ['_', '_', '_', '_'],
@@ -15,7 +15,7 @@ def detect_alive_block(field):
         for j in range(0, field.shape[1]):
             if field[i][j] == '*':
                 middle_elem = field[i + 1][i + 1]
-                return field[i:i + 3, j:j + 3]
+                return field[i-1:i + 2, j-1:j + 2]
 
 
 def print_matrix(matrix):
@@ -25,6 +25,7 @@ def print_matrix(matrix):
         print()
 
 
+"Печать первого живого блока"
 print_matrix(detect_alive_block(game_zone))
 
 
@@ -33,7 +34,7 @@ def step(matrix, neighbours=0):
         for j in range(0, matrix.shape[1]):
             if matrix[i][j] == '*':
                 neighbours += 1
-    if (matrix[1][1] == '*') and (neighbours < 3 or neighbours > 5):
+    if (matrix[1][1] == '*') and (neighbours < 3 or neighbours > 4):
         matrix[1][1] = '_'
     elif (matrix[1][1] == '_') and (neighbours == 3):
         matrix[1][1] = '*'
@@ -41,6 +42,7 @@ def step(matrix, neighbours=0):
     return matrix
 
 
+"Полная печать"
 print_matrix(step(detect_alive_block(game_zone)))
 
 #         "Таймер где-то тут"
